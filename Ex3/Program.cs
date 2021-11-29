155816f50bc77aff98d3d9523bf8842b73bef3b0
morm934 <morm934@gmail.com>
2021-11-29 13:39:50 +0300

    ex3-5 added

---
+++ Ex3/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace Ex3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите строку");
+            string inputStr = Console.ReadLine();
+
+            int CountN = 0;
+            bool DecimateFounded = false;
+
+            for (int i = 0; i < inputStr.Length; i++) 
+            {
+
+                if (inputStr[i] == 'н' && !DecimateFounded ) 
+                {
+                    CountN = CountN + 1;
+                }
+                if (!DecimateFounded && inputStr[i] == ',')
+                {
+                    DecimateFounded = true;
+                }
+            }
+
+
+
+            CountN = (DecimateFounded) ? CountN : 0;  //закоментить если в предложения без запятых не может быть.
+
+            Console.WriteLine($"Количество букв 'н' до 1 запятой равно {CountN}");
+
+        }
+    }
+}
